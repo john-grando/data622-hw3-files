679676ef25a4a62f5b03a6cc6e2ce6eb390c3950
jgrando <john.grando@gmail.com>
2018-10-16 22:24:24 -0400

    updated error message

--- pull_data.py
+++ pull_data.py
@@ -75,7 +75,7 @@ def main():
         logger.info("download test file")
         get_kaggle_data(test_url, get_credentials(), 'test_data.csv')
     else:
-        logger.error("The credentials.txt file is missing, please add a credentials.txt file in the HiddenFiles folder as UserName,Password and uncomment the COPY HiddenFiles/credentials.txt line in the Dockerfile")
+        logger.error("The credentials.txt file is missing, please add a credentials.txt file in the HiddenFiles folder as UserName,Password and uncomment the COPY HiddenFiles/credentials.txt line in the Dockerfile (if using docker).")
         sys.exit()
     return
 
